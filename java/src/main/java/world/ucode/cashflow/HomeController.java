3ed1da5c663b69e0cd8ef53599cdffebcef5fe33
nnocturnnn <vikchehovich@gmail.com>
2021-02-18 19:52:25 +0200

    java auth

---
+++ java/src/main/java/world/ucode/cashflow/HomeController.java
@@ -0,0 +1,14 @@
+package world.ucode.cashflow;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		return "home";
+	}
+}
---
+++ java/src/main/java/world/ucode/cashflow/RegisterController.java
@@ -0,0 +1,14 @@
+package world.ucode.cashflow;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class RegisterController {
+
+	@GetMapping("/register")
+	public String index(Model model) {
+		return "register";
+	}
+}
