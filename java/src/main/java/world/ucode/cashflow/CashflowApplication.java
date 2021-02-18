ccae7eb94a0e380edcc04246af66d50a993dc16c
nnocturnnn <vikchehovich@gmail.com>
2021-02-18 18:35:54 +0200

    all

---
+++ java/src/main/java/world/ucode/cashflow/CashflowApplication.java
@@ -0,0 +1,18 @@
+package world.ucode.cashflow;
+
+import java.util.Arrays;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CashflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowApplication.class, args);
+	}
+}
