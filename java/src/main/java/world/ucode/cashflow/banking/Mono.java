9b9e1691f4a38415f8db82e24f64e012ede2834c
nnnocturn <vikchehovich@gmail.com>
2021-02-28 23:35:36 +0200

    secure

--- java/src/main/java/world/ucode/cashflow/banking/Mono.java
+++ java/src/main/java/world/ucode/cashflow/banking/Mono.java
@@ -1,43 +1,43 @@
-package world.ucode.cashflow.banking;
+// package world.ucode.cashflow.banking;
 
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+// import java.net.URI;
+// import java.net.URLEncoder;
+// import java.net.http.HttpClient;
+// import java.net.http.HttpRequest;
+// import java.net.http.HttpResponse;
+// import java.nio.charset.StandardCharsets;
+// import java.util.HashMap;
+// import java.util.Map;
 
-public class Java11HttpClientExample {
+// public class Java11HttpClientExample {
 
-    private final HttpClient httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-            .build();
+//     private final HttpClient httpClient = HttpClient.newBuilder()
+//             .version(HttpClient.Version.HTTP_2)
+//             .build();
 
-    public static void main(String[] args) throws Exception {
+//     public static void main(String[] args) throws Exception {
 
-        Java11HttpClientExample obj = new Java11HttpClientExample();
+//         Java11HttpClientExample obj = new Java11HttpClientExample();
 
-        System.out.println("Testing 1 - Send Http GET request");
-        obj.sendGet();
+//         System.out.println("Testing 1 - Send Http GET request");
+//         obj.sendGet();
 
-    }
+//     }
 
-    private void sendGet() throws Exception {
+//     private void sendGet() throws Exception {
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://api.monobank.ua/personal/client-info"))
-                .setHeader("X-Token", "u9z0t-MyklvB8QOuNxjWaf9wAPe93Uy_eQEAEsFYrZdM")
-                .build();
+//         HttpRequest request = HttpRequest.newBuilder()
+//                 .GET()
+//                 .uri(URI.create("https://api.monobank.ua/personal/client-info"))
+//                 .setHeader("X-Token", "u9z0t-MyklvB8QOuNxjWaf9wAPe93Uy_eQEAEsFYrZdM")
+//                 .build();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+//         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.statusCode());
+//         System.out.println(response.statusCode());
 
-        System.out.println(response.body());
+//         System.out.println(response.body());
 
-    }
+//     }
 
-}
+// }
