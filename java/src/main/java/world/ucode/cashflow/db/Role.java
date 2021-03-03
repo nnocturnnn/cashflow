b6173d95913c54dfdd62374978c8cc26835ca01c
nnocturnnn <vikchehovich@gmail.com>
2021-03-04 00:38:44 +0200

    re_role

--- java/src/main/java/world/ucode/cashflow/db/Role.java
+++ java/src/main/java/world/ucode/cashflow/db/Role.java
@@ -1,6 +1,12 @@
 package world.ucode.cashflow.db;
 
+import org.springframework.security.core.GrantedAuthority;
 
-public enum Role {
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
