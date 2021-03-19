6e77becaf7bda9c66c6b3ab92ddcaa61ae83da80
nnocturnnn <vikchehovich@gmail.com>
2021-03-19 18:16:53 +0200

    json

--- java/src/main/java/world/ucode/cashflow/banking/MonoTransaction.java
+++ java/src/main/java/world/ucode/cashflow/banking/MonoTransaction.java
@@ -12,15 +12,15 @@ public class MonoTransaction {
     private String time;
     private String description;
     private String mcc;
-    private String shit2;
-    private String text;
-    private String type;
-    private String currency;
-    private String shit3;
-    private String shit4;
+    private String originalMcc;
+    private String amount;
+    private String operationAmount;
+    private String currencyCode;
+    private String commissionRate;
+    private String cashbackAmount;
     private String balance;
-    private String tag;
-    private String shit5;
-    private String shit6;
-    private String wallet;
+    private String hold;
+    private String receiptId;
+    private String counterEdrpou;
+    private String counterIban;
 }   
